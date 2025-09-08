f71f5dd7792fcce1191a65252381fd5fcba9741e
mulatta <67085791+mulatta@users.noreply.github.com>
2025-09-08 21:09:02 +0900

    chore: format code

--- terraform/gitlab/repo-mirror.tf
+++ terraform/gitlab/repo-mirror.tf
@@ -11,7 +11,7 @@ resource "gitlab_project" "repos" {
   name                                = element(split("/", each.key), 1)
   import_url                          = "https://github.com/${each.key}"
   mirror                              = true
-  mirror_trigger_builds                = true
+  mirror_trigger_builds               = true
   mirror_overwrites_diverged_branches = true
   shared_runners_enabled              = false
   visibility_level                    = "public"
