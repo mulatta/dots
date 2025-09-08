22ee594e804288db2153fbff0fde7ddbaa2f193e
mulatta <67085791+mulatta@users.noreply.github.com>
2025-09-08 21:09:02 +0900

    chore(jj): add jujutsu configs

--- home-manager/modules/git/jujutsu.nix
+++ home-manager/modules/git/jujutsu.nix
@@ -1,10 +1,45 @@
 { config, ... }:
+let
+  gitCfg = config.programs.git;
+in
 {
   programs.jujutsu = {
     enable = true;
     settings = {
-      user.email = config.programs.git.userEmail;
-      user.name = config.programs.git.userName;
+      user.email = gitCfg.userEmail;
+      user.name = gitCfg.userName;
+      ui = {
+        pager = "less -FRX";
+        show-cryptographix-signatures = true;
+        default-command = [
+          "log"
+          "--reversed"
+        ];
+      };
+
+      aliases = {
+        l = [
+          "log"
+          "-r"
+          "all()"
+          "--template"
+          "builtin_log_compact"
+        ];
+        s = [ "show" ];
+        d = [ "describe" ];
+      };
+
+      signing = {
+        backend = "ssh";
+        behaviour = "own";
+        key = gitCfg.signing.key;
+      };
+
+      template-aliases = {
+        "format_short_signature(signature)" = "signature.name()";
+        "format_timestamp(timestamp)" = "timestamp.ago()";
+        "format_short_id(id)" = "id.shortest(8)";
+      };
     };
   };
 }
