573047135d0d7c1960e4c82449b38a92634e52ed
mulatta <67085791+mulatta@users.noreply.github.com>
2025-09-09 10:43:20 +0900

    chore: format add .sops.yaml rules

--- shells/flake-module.nix
+++ shells/flake-module.nix
@@ -1,17 +1,16 @@
 {
-  perSystem =
-    {
-      inputs',
-      pkgs,
-      ...
-    }:
-    {
-      devShells.default = pkgs.mkShellNoCC {
-        nativeBuildInputs = [
-          inputs'.clan-core.packages.default
-          inputs'.clan-core.packages.clan-app
-          pkgs.sops
-        ];
-      };
+  perSystem = {
+    inputs',
+    pkgs,
+    ...
+  }: {
+    devShells.default = pkgs.mkShellNoCC {
+      nativeBuildInputs = [
+        inputs'.clan-core.packages.default
+        inputs'.clan-core.packages.clan-app
+        pkgs.sops
+        pkgs.ssh-to-age
+      ];
     };
+  };
 }
