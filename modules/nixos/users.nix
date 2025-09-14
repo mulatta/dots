58193c6c1908cd6c36ec69618e4c7123b860ccc4
mulatta <67085791+mulatta@users.noreply.github.com>
2025-09-14 23:32:04 +0900

    feat(modules): add users.nix

---
+++ modules/nixos/users.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  clan.core.vars.generators.mulatta-password = {
+    files.password-hash.neededFor = "users";
+    files.password.deploy = false;
+    runtimeInputs = [pkgs.mkpasswd pkgs.xkcdpass];
+    prompts.password.type = "hidden";
+    script = ''
+       prompt_value="$(cat "$prompts"/password)"
+      if [[ -n "''${prompt_value-}" ]]; then
+        echo "$prompt_value" | tr -d "\n" > "$out"/password
+      else
+        xkcdpass --numwords 4 --delimiter - --count 1 | tr -d "\n" > "$out"/password
+      fi
+      mkpasswd -s -m sha-512 < "$out"/password | tr -d "\n" > "$out"/password-hash
+    '';
+  };
+}
