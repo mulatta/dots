7821bfcc3b581cb307d207697a3c6d4663c90339
mulatta <67085791+mulatta@users.noreply.github.com>
2025-09-15 00:17:40 +0900

    remove .sops.yaml
    - Use clan-core instead

--- machines/mulatta/configuration.nix
+++ machines/mulatta/configuration.nix
@@ -16,24 +16,6 @@
   ];
 
   clan.core.networking.targetHost = lib.mkForce "root@mulatta.local";
-  clan.core.vars.generators.mulatta-password = {
-    files.password-hash.neededFor = "users";
-    files.password.deploy = false;
-    runtimeInputs = [
-      pkgs.mkpasswd
-      pkgs.xkcdpass
-    ];
-    prompts.password.type = "hidden";
-    script = ''
-       prompt_value="$(cat "$prompts"/password)"
-      if [[ -n "''${prompt_value-}" ]]; then
-        echo "$prompt_value" | tr -d "\n" > "$out"/password
-      else
-        xkcdpass --numwords 4 --delimiter - --count 1 | tr -d "\n" > "$out"/password
-      fi
-      mkpasswd -s -m sha-512 < "$out"/password | tr -d "\n" > "$out"/password-hash
-    '';
-  };
 
   networking.hostName = "mulatta";
 
