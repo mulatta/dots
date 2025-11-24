822c6770adf38850f6cecc34064ee78e2ae43dbf
mulatta <67085791+mulatta@users.noreply.github.com>
2025-11-24 11:02:01 +0900

    add vscode

--- home-manager/modules/vscode/default.nix
+++ home-manager/modules/vscode/default.nix
@@ -1,10 +1,8 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   programs.vscode = {
     enable = true;
     profiles.default = {
-      extensions =
-        with pkgs.vscode-extensions;
+      extensions = with pkgs.vscode-extensions;
         [
           ms-python.python
           ms-python.vscode-pylance
