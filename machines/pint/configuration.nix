a8ed4d8ba6fcd687c62b2f0dcb2b14cf4c40f474
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-02 13:44:36 +0900

    feat(pint): enable raspberry-pi direct kernel boot
    Configure Raspberry Pi 5 to use custom direct boot module:
    - Enable hardware.raspberry-pi.boot with config.txt options
    - Disable systemd initrd (srvos default, incompatible with RPi)
    - Add required initrd modules: pcie_brcmstb, reset-raspberrypi, etc.
    - Enable vc4-kms-v3d-pi5 overlay for display output

--- machines/pint/configuration.nix
+++ machines/pint/configuration.nix
@@ -10,6 +10,7 @@
     self.inputs.srvos.nixosModules.mixins-terminfo
     self.inputs.srvos.nixosModules.mixins-nix-experimental
     self.inputs.disko.nixosModules.disko
+    ../../nixosModules/raspberry-pi
     ../../nixosModules/users.nix
     ../../nixosModules/zerotier.nix
     ../../nixosModules/dns-client.nix
@@ -17,14 +18,55 @@
     ./modules/network.nix
   ];
 
-  # Raspberry Pi 5 boot loader (extlinux, not GRUB)
-  boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.initrd.systemd.enable = lib.mkForce false;
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "usbhid"
+    "usb_storage"
+    "uas"
+    "vc4"
+    "pcie_brcmstb"
+    "reset-raspberrypi"
+    "nvme"
+  ];
+  boot.kernelParams = [
+    "console=tty1"
+    "console=ttyAMA10,115200"
+  ];
 
-  # raspberry pi kernel doesnot have tpm module
-  boot.initrd.availableKernelModules = lib.mkForce [ ];
+  hardware.raspberry-pi = {
+    boot.enable = true;
+    config.all = {
+      options = {
+        arm_64bit = {
+          enable = true;
+          value = 1;
+        };
+        enable_uart = {
+          enable = true;
+          value = 1;
+        };
+        avoid_warnings = {
+          enable = true;
+          value = 1;
+        };
+        os_check = {
+          enable = true;
+          value = 0;
+        };
+        display_auto_detect = {
+          enable = true;
+          value = 1;
+        };
+        max_framebuffers = {
+          enable = true;
+          value = 2;
+        };
+      };
+      dt-overlays.vc4-kms-v3d-pi5.enable = true;
+    };
+  };
 
-  # Use WireGuard .x domain for clan connectivity
   clan.core.networking.targetHost = "root@pint.x";
 
   nixpkgs.hostPlatform = "aarch64-linux";
@@ -33,8 +75,6 @@
   networking.hostName = "pint";
 
   services.openssh.enable = true;
-
-  # mDNS for local discovery
   services.avahi = {
     enable = true;
     nssmdns4 = true;
