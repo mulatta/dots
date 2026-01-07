4574e638b2533c92e003c16771f6120d073d1fdb
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-07 15:46:10 +0900

    feat(pkgs): add jmt

---
+++ packages/jmt/default.nix
@@ -0,0 +1,38 @@
+{
+  python3,
+  lib,
+  makeWrapper,
+  nix,
+  jujutsu,
+}:
+python3.pkgs.buildPythonApplication {
+  pname = "jmt";
+  version = "0.1.0";
+  pyproject = false;
+
+  src = ./.;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  propagatedBuildInputs = with python3.pkgs; [
+    tomli # For Python < 3.11 compatibility
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm755 jmt.py $out/bin/jmt
+    wrapProgram $out/bin/jmt \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          nix
+          jujutsu
+        ]
+      }
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Format commits with jj fix using flake's treefmt config";
+    mainProgram = "jmt";
+  };
+}
