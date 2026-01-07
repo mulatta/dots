345352f66f0c67ae3ae9e8bcd968dcc6d8251f9f
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-08 02:23:42 +0900

    chore(macos): remove spacedrive from home-manager profile

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -9,7 +9,6 @@
       packages = {
         inherit jmt;
         merge-when-green = pkgs.callPackage ./merge-when-green { inherit jmt; };
-        spacedrive = pkgs.callPackage ./spacedrive { };
         rbw-pinentry = pkgs.callPackage ./rbw-pinentry { };
       }
       // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
