b27ea439f8a1f1beb4e4ab5c87298201436f6352
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-09 21:10:12 +0900

    drop unused fish plugins

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -301,15 +301,6 @@ in
         };
       }
       {
-        name = "completion-sync";
-        src = pkgs.fetchFromGitHub {
-          owner = "iynaix";
-          repo = "fish-completion-sync";
-          rev = "4f058ad2986727a5f510e757bc82cbbfca4596f0";
-          sha256 = "sha256-kHpdCQdYcpvi9EFM/uZXv93mZqlk1zCi2DRhWaDyK5g=";
-        };
-      }
-      {
         name = "autopair";
         src = pkgs.fetchFromGitHub {
           owner = "jorgebucaran";
