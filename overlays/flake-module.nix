07df1120536902ae9cd2a406722f7fc8e1ea67c8
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-14 14:38:24 +0900

    drop nixos-unstable in overlays/flake-module.nix

--- overlays/flake-module.nix
+++ overlays/flake-module.nix
@@ -1,21 +1,9 @@
 { inputs, ... }:
 {
   flake.overlays = {
-    default =
-      _final: prev:
-      let
-        unstable = import inputs.nixpkgs-unstable {
-          system = prev.stdenv.hostPlatform.system;
-          config = prev.config;
-        };
-      in
-      {
-        inherit unstable;
-        zjstatus = inputs.zjstatus.packages.${prev.stdenv.hostPlatform.system}.default;
-
-        # Use nixpkgs-unstable vaultwarden 1.35.1+ (SSO support added in 1.35.0)
-        vaultwarden = unstable.vaultwarden;
-      };
+    default = _final: prev: {
+      zjstatus = inputs.zjstatus.packages.${prev.stdenv.hostPlatform.system}.default;
+    };
   };
 
   perSystem =
