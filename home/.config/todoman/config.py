2fda772ab9421af7169898e49a30692c89a2c7dc
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-20 00:10:51 +0900

    pim: migrate CalDAV/CardDAV from Nextcloud to Stalwart
    - vdirsyncer: switch remote storage to Stalwart DAV endpoints
    - khal: update calendar path to stalwart directory
    - khard: update contacts path to stalwart directory  
    - todoman: update path and add humanize option

--- home/.config/todoman/config.py
+++ home/.config/todoman/config.py
@@ -1,6 +1,7 @@
-path = "/Users/seungwon/.local/share/calendars/*/*"
+path = "/Users/seungwon/.local/share/calendars/stalwart/*"
 date_format = "%Y-%m-%d"
 time_format = "%H:%M"
-default_list = "personal"
+default_list = "default"
 default_due = 0
+humanize = True
 
