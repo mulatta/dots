6c4831263ad5a904067f0ae932a14c6d88dddf1a
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-23 12:52:16 +0900

    fix clan sshd darwinModule

--- nixosModules/ssh.nix
+++ nixosModules/ssh.nix
@@ -1,8 +1,11 @@
 {
-  programs.ssh.knownHosts.ssh-ca.extraHostNames = [
-    "*.x" # WireGuard mesh
-    "*.i" # ZeroTier internal
-  ];
+  programs.ssh.knownHosts.ssh-ca = {
+    publicKeyFile = ../vars/shared/openssh-ca/id_ed25519.pub/value;
+    extraHostNames = [
+      "*.x" # WireGuard mesh
+      "*.i" # ZeroTier internal
+    ];
+  };
 
   programs.ssh.extraConfig = ''
     # Use certificate-based authentication for mesh networks
