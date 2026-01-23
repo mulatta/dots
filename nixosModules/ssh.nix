95eaf193a0a3544f004294ccdeaf176b87456f70
Seungwon Lee <67085791+mulatta@users.noreply.github.com>
2026-01-24 00:12:58 +0900

    fix clan sshd nixosModule - XOR assertion in publicKey / publicKeyFile (#223)

--- nixosModules/ssh.nix
+++ nixosModules/ssh.nix
@@ -1,12 +1,4 @@
 {
-  programs.ssh.knownHosts.ssh-ca = {
-    publicKeyFile = ../vars/shared/openssh-ca/id_ed25519.pub/value;
-    extraHostNames = [
-      "*.x" # WireGuard mesh
-      "*.i" # ZeroTier internal
-    ];
-  };
-
   programs.ssh.extraConfig = ''
     # Use certificate-based authentication for mesh networks
     Host *.x *.i
