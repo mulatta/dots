005618110356a6b60bccaa5c766f68818fcedd1f
Seungwon Lee <67085791+mulatta@users.noreply.github.com>
2026-01-26 23:45:38 +0900

    fix radicle node config (#261)
    * add auto-upgrade on malt and pint
    
    * fix radicle connectNode configuration

--- machines/malt/configuration.nix
+++ machines/malt/configuration.nix
@@ -9,6 +9,7 @@
     self.inputs.srvos.nixosModules.server
     self.inputs.disko.nixosModules.disko
     ../../nixosModules/disko-zfs.nix
+    ../../nixosModules/auto-upgrade.nix
     ../../nixosModules/radicle-mulatta.nix
     ./modules/cognee.nix
     ./modules/immich.nix
