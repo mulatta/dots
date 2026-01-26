a6c5d687d6ca5a99bdad4fd4404efff8616dba9c
Seungwon Lee <67085791+mulatta@users.noreply.github.com>
2026-01-26 21:13:46 +0900

    add llm utilities (#256)
    * drop thunderbird
    
    * bump llm-agents.nix
    
    * add llm utilities

--- home-manager/modules/mail/aerc.nix
+++ home-manager/modules/mail/aerc.nix
@@ -1,22 +1,8 @@
 { pkgs, ... }:
-let
-  open-in-thunderbird = pkgs.writeShellScriptBin "open-in-thunderbird" ''
-    tmpfile=$(mktemp).eml
-    cat > "$tmpfile"
-    ${
-      if pkgs.stdenv.isDarwin then
-        "/usr/bin/open -a Thunderbird \"$tmpfile\""
-      else
-        "${pkgs.thunderbird}/bin/thunderbird \"$tmpfile\""
-    }
-    (sleep 10 && rm -f "$tmpfile") &
-  '';
-in
 {
   home.packages = [
     pkgs.aerc
     pkgs.mblaze
     pkgs.w3m
-    open-in-thunderbird
   ];
 }
