3423beb15b05ac9fbdbcfc6f31189b3037104cd6
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-20 17:06:57 +0900

    fix home brew path

--- darwinModules/homebrew.nix
+++ darwinModules/homebrew.nix
@@ -8,7 +8,7 @@
   homebrew.enable = true;
   system.activationScripts.homebrew.text = lib.mkIf config.homebrew.enable (
     lib.mkBefore ''
-      if [[ ! -f "${config.homebrew.brewPrefix}/brew" ]]; then
+      if [[ ! -f "${config.homebrew.prefix}/bin/brew" ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       fi
     ''
