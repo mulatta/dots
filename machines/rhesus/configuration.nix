140181e920e18d06d9535689f0939b7a75e91f1c
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-23 14:59:27 +0900

    rhesus: drop colima/docker and linux-builder

--- machines/rhesus/configuration.nix
+++ machines/rhesus/configuration.nix
@@ -11,10 +11,8 @@
     self.inputs.srvos.darwinModules.mixins-nix-experimental
     self.inputs.srvos.darwinModules.mixins-terminfo
     ../../darwinModules/desktop.nix
-    ../../darwinModules/docker.nix
     ../../darwinModules/homebrew.nix
     ../../darwinModules/karabiner.nix
-    ../../darwinModules/linux-builder.nix
     ../../darwinModules/remote-builder.nix
     ../../darwinModules/nix-daemon.nix
     ../../darwinModules/nix-index.nix
