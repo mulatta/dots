7a34201af8a2c00d7c3c515e146cbb6d27c76279
mulatta <67085791+mulatta@users.noreply.github.com>
2026-08-06 16:46:03 +0900

    rhesus: authenticate Nix gRPC builder with mTLS

--- darwinModules/remote-builder.nix
+++ darwinModules/remote-builder.nix
@@ -6,6 +6,12 @@
 }:
 let
   grpcSupported = !lib.hasInfix "pre" config.nix.package.version;
+  certs = config.sops.secrets;
+  grpcUri =
+    "grpc://10.100.0.2:50051"
+    + "?ca-cert=${certs.nix-grpc-ca-cert.path}"
+    + "&client-cert=${certs.nix-grpc-client-cert.path}"
+    + "&client-key=${certs.nix-grpc-client-key.path}";
 in
 {
   # nix-grpc-store only ships a NixOS client module, but it only extends
@@ -14,11 +20,26 @@ in
 
   programs.nix-grpc-store.enable = grpcSupported;
 
+  sops.secrets = {
+    nix-grpc-ca-cert = {
+      sopsFile = ../sops/rhesus.yaml;
+      mode = "0444";
+    };
+    nix-grpc-client-cert = {
+      sopsFile = ../sops/rhesus.yaml;
+      mode = "0444";
+    };
+    nix-grpc-client-key = {
+      sopsFile = ../sops/rhesus.yaml;
+      mode = "0400";
+    };
+  };
+
   nix.distributedBuilds = true;
 
   nix.buildMachines = lib.optionals grpcSupported [
     {
-      hostName = "grpc://10.100.0.2:50051?insecure=1";
+      hostName = grpcUri;
       protocol = null;
       systems = [ "x86_64-linux" ];
       maxJobs = 24;
