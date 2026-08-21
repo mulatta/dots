426220805232ab9148e58d8f97d718c432b54ee2
mulatta <67085791+mulatta@users.noreply.github.com>
2026-08-21 21:31:24 +0900

    machines: obtain network identity from home router
    Let the home router own address and gateway assignment so malt and pint keep reserved identities without duplicating subnet state on each host. Accept upstream resolvers from DHCP while existing split-DNS configuration continues to route private domains.

--- machines/malt/modules/network.nix
+++ machines/malt/modules/network.nix
@@ -1,25 +1,6 @@
 {
   networking = {
     useDHCP = false;
-
-    interfaces.enp1s0 = {
-      useDHCP = false;
-      ipv4.addresses = [
-        {
-          address = "10.80.169.67";
-          prefixLength = 24;
-        }
-      ];
-    };
-
-    defaultGateway = {
-      address = "10.80.169.254";
-      interface = "enp1s0";
-    };
-
-    nameservers = [
-      "117.16.191.6"
-      "168.126.63.1"
-    ];
+    interfaces.enp1s0.useDHCP = true;
   };
 }
