59d2cc1ddcbd61d0479b34288bdd7788d05781e3
mulatta <67085791+mulatta@users.noreply.github.com>
2026-08-30 00:44:32 +0900

    dns: authorize HE secondary transfers
    Store the externally coordinated Knot key in the Clan SOPS secret store and scope its TSIG capability to public-zone transfers only.

--- machines/cask/modules/knot/default.nix
+++ machines/cask/modules/knot/default.nix
@@ -6,21 +6,52 @@
 }:
 let
   durePackages = self.inputs.dure.packages.${pkgs.stdenv.hostPlatform.system};
-  publicIPv4 = config.networking.cask.ipv4.address;
+  ipv4 = config.networking.cask.ipv4.address;
   publicZone = pkgs.writeText "mulatta.io.zone" (
-    builtins.replaceStrings [ "@SERVICE_IP@" ] [ publicIPv4 ] (builtins.readFile ./mulatta.io.zone)
+    builtins.replaceStrings [ "@SERVICE_IP@" ] [ ipv4 ] (builtins.readFile ./mulatta.io.zone)
   );
 in
 {
+  sops.secrets."knot-keys.conf" = {
+    owner = "knot";
+    restartUnits = [ "knot.service" ];
+  };
+
   services.knot = {
     enable = true;
+    keyFiles = [ config.sops.secrets."knot-keys.conf".path ];
     settings = {
-      server.listen = [ "${publicIPv4}@53" ];
+      server.listen = [ "${ipv4}@53" ];
+
+      remote = [
+        {
+          id = "he_ip4";
+          address = "216.218.130.2";
+        }
+      ];
+
+      acl = [
+        {
+          id = "he_acl";
+          key = "52cc052c-fb0c-444c-9056-17c32c18d9bb.uniq.mulatta.io";
+          action = "transfer";
+        }
+      ];
 
       template = [
         {
           id = "default";
           semantic-checks = "on";
+        }
+        {
+          id = "master";
+          semantic-checks = "on";
+          notify = [ "he_ip4" ];
+          acl = [ "he_acl" ];
+          zonefile-sync = "-1";
+          zonefile-load = "difference-no-serial";
+          serial-policy = "dateserial";
+          journal-content = "all";
         }
       ];
 
@@ -38,6 +69,7 @@ in
         {
           domain = "mulatta.io";
           file = "${publicZone}";
+          template = "master";
         }
       ];
     };
