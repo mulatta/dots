07529b6aeb999589fbad4924480969f1e17c3bd8
mulatta <67085791+mulatta@users.noreply.github.com>
2026-08-30 00:55:33 +0900

    dns: serve cask primary over IPv6
    Bind Knot and publish ns1 on the stable Vultr IPv6 address so primary authority remains directly reachable across either address family.

--- machines/cask/modules/network.nix
+++ machines/cask/modules/network.nix
@@ -29,6 +29,26 @@ in
         description = "Vultr public IPv4 gateway.";
       };
     };
+
+    ipv6 = {
+      address = lib.mkOption {
+        type = lib.types.str;
+        default = "2401:c080:1c01:382:5400:6ff:fe9c:ad9d";
+        description = "Stable public IPv6 address.";
+      };
+
+      prefixLength = lib.mkOption {
+        type = lib.types.ints.between 0 128;
+        default = 64;
+        description = "Public IPv6 prefix length.";
+      };
+
+      gateway = lib.mkOption {
+        type = lib.types.str;
+        default = "fe80::fc00:6ff:fe9c:ad9d";
+        description = "Vultr link-local IPv6 gateway.";
+      };
+    };
   };
 
   config.networking = {
@@ -39,12 +59,21 @@ in
           inherit (cfg.ipv4) address prefixLength;
         }
       ];
+      ipv6.addresses = [
+        {
+          inherit (cfg.ipv6) address prefixLength;
+        }
+      ];
     };
     defaultGateway = {
       address = cfg.ipv4.gateway;
       interface = cfg.interface;
       source = cfg.ipv4.address;
       metric = 100;
+    };
+    defaultGateway6 = {
+      address = cfg.ipv6.gateway;
+      interface = cfg.interface;
     };
     firewall.allowedUDPPorts = [ 51820 ];
   };
